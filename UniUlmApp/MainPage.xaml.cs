c21e213bb4a7953882b92312080e5e0048e71959
Martin Rauscher <Hades32@gmail.com>
2010-11-22 11:27:28 +0100

    initial commit loading of Mensaplan works
    Signed-off-by: Martin Rauscher <Hades32@gmail.com>

---
+++ UniUlmApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using Microsoft.Phone.Controls;
+
+namespace UniUlmApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Konstruktor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Daten für die ViewModel-Elemente laden
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var mp = new Mensaplan("http://www.uni-ulm.de/mensaplan/mensaplan.xml");
+            mp.Loaded += new Action<Mensaplan>(mensaplan_Loaded);
+        }
+
+        void mensaplan_Loaded(Mensaplan mp)
+        {
+            this.DayPivot.ItemsSource = mp.Tage;
+            var heuteItem = mp.Tage.Where(tag => tag.DateName == "Heute").FirstOrDefault();
+            if (heuteItem != null)
+                this.DayPivot.SelectedItem = heuteItem;
+        }
+    }
+
+    public class DebugConverter : IValueConverter
+    {
+
+        public Object Convert(Object value, Type targetType, Object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            //set a breakpoint here
+            return value;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            //set a breakpoint here
+            return value;
+        }
+    }
+}
---
+++ UniUlmApp/Mensaplan.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace UniUlmApp
+{
+
+
+    public class Mensaplan
+    {
+        public IList<Tag> Tage { get; private set; }
+
+        public event Action<Mensaplan> Loaded;
+
+        public Mensaplan(string url)
+        {
+            this.Loaded += (_) => { };
+            this.Tage = new List<Tag>();
+            var web = new WebClient();
+            web.OpenReadCompleted += new OpenReadCompletedEventHandler(web_OpenReadCompleted);
+            web.OpenReadAsync(new Uri(url));
+        }
+
+        void web_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Error == null)
+                {
+                    var plan = XDocument.Load(e.Result);
+
+                    var tage = plan.Root.Elements("week").Elements("day");
+
+                    foreach (var tag in tage)
+                    {
+                        var date = formatDate(tag.Attribute("date").Value);
+
+                        if (tag.Attribute("open").Value == "1")
+                        {
+                            this.Tage.Add(new Tag()
+                            {
+                                DateName = date,
+                                FullDate = formatDateLong(tag.Attribute("date").Value),
+                                Essen = tag.Elements("meal")
+                                    .Select(x => new Essen() { Typ = x.Attribute("type").Value, Name = x.Value })
+                            });
+                        }
+                        else
+                            this.Tage.Add(new Tag() { DateName = date, Essen = generateNoEssenList() });
+                    }
+                    this.Loaded(this);
+                }
+                else
+                {
+                    MessageBox.Show("Es gab ein Problem. Versuch es später nochmal!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Es gab ein Problem. Versuch es später nochmal!");
+            }
+        }
+
+        private static string formatDate(string date)
+        {
+            var heutedate = DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day;
+            var morgendt = DateTime.Now.AddDays(1);
+            var morgendate = morgendt.Year + "-" + morgendt.Month + "-" + morgendt.Day;
+
+            if (date == heutedate)
+                date = "Heute";
+            else if (date == morgendate)
+                date = "Morgen";
+            else
+            {
+                var dt = DateTime.Parse(date);
+                date = dt.ToString("dddd");
+            }
+
+            return date;
+        }
+
+        private static string formatDateLong(string date)
+        {
+            var dt = DateTime.Parse(date);
+
+            return dt.ToShortDateString();
+        }
+
+        private IEnumerable<Essen> generateNoEssenList()
+        {
+            return new List<Essen>() { new Essen() { Typ = "Heute ist leider", Name = "Geschlossen" } };
+        }
+
+        public class Tag : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public Tag()
+            {
+                //avoid null checks
+                this.PropertyChanged += (_, __) => { };
+            }
+
+            private string _Name;
+
+            public string DateName
+            {
+                get { return this._Name; }
+                set
+                {
+                    if (this._Name != value)
+                    {
+                        this._Name = value;
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("DateName"));
+                    }
+                }
+            }
+
+            private string _FullDate;
+
+            public string FullDate
+            {
+                get { return this._FullDate; }
+                set
+                {
+                    if (this._FullDate != value)
+                    {
+                        this._FullDate = value;
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("DateName"));
+                    }
+                }
+            }
+
+            private IEnumerable<Essen> _Essen;
+
+            public IEnumerable<Essen> Essen
+            {
+                get { return this._Essen; }
+                set
+                {
+                    if (this._Essen != value)
+                    {
+                        this._Essen = value;
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Essen"));
+                    }
+                }
+            }
+
+            public override string ToString()
+            {
+                return this.DateName;
+            }
+        }
+
+        public class Essen : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public Essen()
+            {
+                //avoid null checks
+                this.PropertyChanged += (_, __) => { };
+            }
+
+            private string _Name;
+
+            public string Name
+            {
+                get { return this._Name; }
+                set
+                {
+                    if (this._Name != value)
+                    {
+                        this._Name = value;
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+                    }
+                }
+            }
+
+            private string _Typ;
+
+            public string Typ
+            {
+                get { return this._Typ; }
+                set
+                {
+                    if (this._Typ != value)
+                    {
+                        this._Typ = value;
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Typ"));
+                    }
+                }
+            }
+
+        }
+    }
+}
