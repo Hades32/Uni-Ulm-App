2dc52ac8583b4263c4fbfcb3ba364793fd43b6e0
Martin Rauscher <Hades32@gmail.com>
2011-05-25 14:41:00 +0200

    Added logo to splashscreen and then removed it alltogether to speed up application loading :D added some helper stuff
    Signed-off-by: Martin Rauscher <Hades32@gmail.com>

---
+++ UniUlmApp/UIHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UniUlmApp
+{
+    public static class UIHelper
+    {
+        public static void SetTimeout(int milliseconds, Action func)
+        {
+            var timer = new DispatcherTimerContainingAction
+            {
+                Interval = new TimeSpan(0, 0, 0, 0, milliseconds),
+                Action = func
+            };
+            timer.Tick += _onTimeout;
+            timer.Start();
+        }
+
+        private static void _onTimeout(object sender, EventArgs arg)
+        {
+            var t = sender as DispatcherTimerContainingAction;
+            t.Stop();
+            t.Action();
+            t.Tick -= _onTimeout;
+        }
+    }
+
+    public class DispatcherTimerContainingAction : System.Windows.Threading.DispatcherTimer
+    {
+        /// <summary>
+        /// uncomment this to see when the DispatcherTimerContainingAction is collected
+        /// if you remove  t.Tick -= _onTimeout; line from _onTimeout method
+        /// you will see that the timer is never collected
+        /// </summary>
+        //~DispatcherTimerContainingAction()
+        //{
+        //    throw new Exception("DispatcherTimerContainingAction is disposed");
+        //}
+
+        public Action Action { get; set; }
+    }
+}
